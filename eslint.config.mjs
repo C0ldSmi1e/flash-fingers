10a30b237e59e8091f8f4bea19a1aa47fb4244a4
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-07-03 18:11:08 -0700

    update: eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     "ignores": [
-      "components/ui/*",
+      "src/components/ui/*",
       "lib/utils.ts"
     ],
     "rules": {
